01dcd85d21d36ff25864f060c37957482b648f0d
harrycao911 <giahycao@gmail.com>
2023-05-04 12:24:52 +1000

    Update Sidebar.js

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -58,7 +58,9 @@ const Sidebar = (props) => {
             </Link></li>
           <li className="nav-item nav-toggle">
             <Link className="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
-              <i className="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i className="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary" data-ticon="icon-disc"></i></Link>
+              <i className="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i>
+              <i className="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary" data-ticon="icon-disc"></i>
+            </Link>
           </li>
         </ul>
       </div>
